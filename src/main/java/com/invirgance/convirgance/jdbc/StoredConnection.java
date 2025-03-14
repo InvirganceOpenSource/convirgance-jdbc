7932bcf42c97bc59184f20ed5ce400bc273b330d
ethanivg <ethan.henry@invirgance.com>
2025-03-14 11:26:54 -0500

    Docs: StoredConnection

--- src/main/java/com/invirgance/convirgance/jdbc/StoredConnection.java
+++ src/main/java/com/invirgance/convirgance/jdbc/StoredConnection.java
@@ -33,7 +33,9 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 /**
- *
+ * Represents a customized stored connection for interacting with databases.
+ * Use the StoredConnections Builder class to create this.
+ * 
  * @author jbanes
  */
 public class StoredConnection
@@ -47,16 +49,33 @@ public class StoredConnection
         this.database = database;
     }
     
+    /**
+     * Returns the name that this StoredConnection is recognized by.
+     * 
+     * @return The name.
+     */
     public String getName()
     {
         return record.getString("name");
     }
     
+    /**
+     * Returns the driver (AutomaticDriver) in use.
+     * Use the returned object to update the behavior of this driver along with its DataSource, Artifacts etc...
+     * 
+     * @return The driver.
+     */
     public AutomaticDriver getDriver()
     {
         return AutomaticDrivers.getDriverByName(record.getString("driver"));
     }
     
+    /**
+     * Returns the current driver configuration.
+     * Use this to update/retrieve driver information such as the username, password or URl.
+     * 
+     * @return The drivers configuration.
+     */
     public DriverConfig getDriverConfig()
     {
         if(record.isNull("driverConfig")) record.put("driverConfig", new JSONObject());
@@ -64,6 +83,13 @@ public class StoredConnection
         return new DriverConfig(record.getJSONObject("driverConfig"));
     }
     
+    /**
+     * Returns the current driver's data source configuration.
+     * If no configuration exists the DataSourceConfig will be created using the 
+     * defaults for ALL properties.
+     * 
+     * @return The data source configuration.
+     */
     public DataSourceConfig getDataSourceConfig()
     {
         DataSource source = getDriver().getDataSource();
@@ -74,6 +100,12 @@ public class StoredConnection
         return new DataSourceConfig(record.getJSONObject("datasourceConfig"), new DataSourceManager(source).getConfig());
     }
     
+    /**
+     * Attempts to get a connection to the database using the data source configuration.
+     * Will throw an exception if the connection fails.
+     * 
+     * @return A connection to the database.
+     */
     public Connection getConnection()
     {
         try
@@ -89,6 +121,7 @@ public class StoredConnection
     /**
      * Obtains a connection to the database, executes the callback, and then
      * closes the connection.
+     * 
      * @param callback a callback containing the logic to execute against the database
      * @throws ConvirganceException if an SQL error occurs
      */
@@ -97,6 +130,13 @@ public class StoredConnection
         ConnectionCallback.execute(getDataSource(), callback);
     }
     
+    /**
+     * Returns the data source for the driver used by this StoredConnection.
+     * If the drivers configuration lacks a data source a new one is assigned 
+     * based on the URL's prefix.
+     * 
+     * @return The data source.
+     */
     public DataSource getDataSource()
     {
         DriverConfig config = getDriverConfig();
@@ -120,6 +160,12 @@ public class StoredConnection
         throw new ConvirganceException("Connection not configured!");
     }
     
+    /**
+     * Returns a new DBMS setup specifically for this StoredConnection.
+     * Used to act on the database.
+     * 
+     * @return A DBMS for executing queries etc...
+     */
     public DBMS getDBMS()
     {
         return new DBMS(getDataSource());
@@ -204,6 +250,9 @@ public class StoredConnection
         }
     }
     
+    /**
+     * Represents the data source configuration for a driver.
+     */
     public class DataSourceConfig
     {
         private JSONObject config;
@@ -215,6 +264,11 @@ public class StoredConnection
             this.defaults = defaults;
         }
         
+        /**
+         * Returns the property names available to the data source.
+         * 
+         * @return A string array of the property names.
+         */
         public String[] getProperties()
         {
             if(!defaults.isEmpty()) defaults.keySet().toArray(String[]::new);
@@ -222,6 +276,14 @@ public class StoredConnection
             return config.keySet().toArray(String[]::new);
         }
         
+        /**
+         * Gets the current value of a property.
+         * If the property is missing from the current configuration the value of the default is returned.
+         * The default value returned will most likely be null, this is the same if the property doesn't exist at all.
+         *  
+         * @param property Property name.
+         * @return The current value.
+         */
         public Object getProperty(String property)
         {
             if(!config.containsKey(property)) return defaults.get(property);
@@ -229,11 +291,24 @@ public class StoredConnection
             return config.get(property);
         }
         
+        /**
+         * Updates the configuration, updating the property's value.
+         * 
+         * @param property Property name.
+         * @param value Value.
+         */
         public void setProperty(String property, String value)
         {
             this.setProperty(property, (Object)value);
         }
         
+        /**
+         * Updates the configuration, updating the property's value.
+         * This preserves the values type, but JSONObject must be able to serialize the object.
+         * 
+         * @param property Property name.
+         * @param value Value.
+         */
         public void setProperty(String property, Object value)
         {
             config.put(property, value);
