f67b5e288ed83e8007c3d4b553d1375d3d76b538
ethanivg <ethan.henry@invirgance.com>
2025-03-14 10:52:09 -0500

    Docs: StoredConnections

--- src/main/java/com/invirgance/convirgance/jdbc/StoredConnections.java
+++ src/main/java/com/invirgance/convirgance/jdbc/StoredConnections.java
@@ -28,7 +28,10 @@ import java.util.Iterator;
 import javax.sql.DataSource;
 
 /**
- *
+ * Used for creating customized reusable database connections.
+ * Allows configuring the driver, the drivers data source, and the StoredConnection itself.
+ * This class utilizes the builder pattern.
+ * 
  * @author jbanes
  */
 public class StoredConnections implements Iterable<StoredConnection>
@@ -50,6 +53,14 @@ public class StoredConnections implements Iterable<StoredConnection>
         return new StoredConnectionBuilder(record, driver);
     }
     
+    /**
+     * Gets the StoredConnection that has the provided name.
+     * This would be used to retrieve a saved StoredConnection.
+     * Example: The user saved a StoredConnection and named it "CoolDatabase".
+     * 
+     * @param name The name assigned to the StoredConnection.
+     * @return The StoredConnection.
+     */
     public static StoredConnection getConnection(String name)
     {
         JSONObject descriptor = database.findDescriptorByName(name);
@@ -85,6 +96,11 @@ public class StoredConnections implements Iterable<StoredConnection>
         };
     }
     
+    /**
+     * A Builder for creating and StoredConnections.
+     * Example: Creating a new StoredConnection for a Driver that was retrieved 
+     * with AutomaticDrivers.getDriverByName().
+     */
     public static class StoredConnectionBuilder
     {
         private JSONObject record;
@@ -96,11 +112,24 @@ public class StoredConnections implements Iterable<StoredConnection>
             this.driver = driver;
         }
         
+        /**
+         * Builds and finalizes the StoredConnection.
+         * At this point you could call 'save()' to preserve it or simply use the StoredConnection within the current run-time.
+         * 
+         * @return The StoredConnection.
+         */
         public StoredConnection build()
         {
             return new StoredConnection(record, database);
         }
         
+        /**
+         * Returns a DriverConfigBuilder for configuring a Driver.
+         * This builder can be used to setup the Driver configuration of a StoredConnection.
+         * Example: Used to set the username, URL and password for the StoredConnection's driver.
+         * 
+         * @return DriverConfigBuilder.
+         */
         public DriverConfigBuilder driver()
         {
             record.put("driverConfig", new JSONObject());
@@ -108,6 +137,12 @@ public class StoredConnections implements Iterable<StoredConnection>
             return new DriverConfigBuilder(this, record.getJSONObject("driverConfig"));
         }
         
+        /**
+         * Returns a builder to setup Driver specific DataSource properties.
+         * Example: setting the 'explicitCachingEnabled' property for the Oracle Thin Driver.
+         * 
+         * @return The DataSourceConfigBuilder.
+         */
         public DataSourceConfigBuilder datasource()
         {
             DataSource source = driver.getDataSource();
@@ -119,6 +154,9 @@ public class StoredConnections implements Iterable<StoredConnection>
         }
     }
     
+    /**
+     * Builds a DriverConfig for a StoredConnectionBuilder.
+     */
     public static class DriverConfigBuilder
     {
         private StoredConnectionBuilder parent;
@@ -130,6 +168,11 @@ public class StoredConnections implements Iterable<StoredConnection>
             this.config = config;
         }
         
+        /**
+         * Builds the StoredConnection with the DriverConfig.
+         * 
+         * @return The StoredConnection.
+         */
         public StoredConnection build()
         {
             if(config.isNull("url") || config.isNull("password"))
@@ -140,6 +183,13 @@ public class StoredConnections implements Iterable<StoredConnection>
             return parent.build();
         }
         
+        /**
+         * Returns the StoredConnectionBuilder for further building.
+         * The connection is not built with this, allowing for further 
+         * configuration such as building a DataSourceConfig.
+         * 
+         * @return The builder.
+         */
         public StoredConnectionBuilder done()
         {
             if(config.isNull("url") || config.isNull("password"))
@@ -150,6 +200,12 @@ public class StoredConnections implements Iterable<StoredConnection>
             return parent;
         }
         
+        /**
+         * Sets the password for the DriverConfig used by the StoredConnection.
+         * 
+         * @param password The password.
+         * @return This builder.
+         */
         public DriverConfigBuilder password(String password)
         {
             config.put("password", password);
@@ -157,6 +213,12 @@ public class StoredConnections implements Iterable<StoredConnection>
             return this;
         }
         
+        /**
+         * Sets the username to use for the DriverConfig of the StoredConnection.
+         * 
+         * @param username The username.
+         * @return This builder.
+         */
         public DriverConfigBuilder username(String username)
         {
             config.put("username", username);
@@ -164,6 +226,12 @@ public class StoredConnections implements Iterable<StoredConnection>
             return this;
         }
         
+        /**
+         * Sets the database URL the DriverConfig will use for the StoredConnection.
+         * 
+         * @param url The database URL.
+         * @return This builder.
+         */
         public DriverConfigBuilder url(String url)
         {
             config.put("url", url);
@@ -172,6 +240,9 @@ public class StoredConnections implements Iterable<StoredConnection>
         }
     }
     
+    /**
+     * Builds the DataSourceConfig for the driver of a StoredConnection.
+     */
     public static class DataSourceConfigBuilder
     {
         private StoredConnectionBuilder parent;
@@ -183,16 +254,36 @@ public class StoredConnections implements Iterable<StoredConnection>
             this.config = config;
         }
         
+        /**
+         * Builds the parent with the options setup by this builder.
+         * 
+         * @return The built StoredConnection.
+         */
         public StoredConnection build()
         {
             return parent.build();
         }
         
+        /**
+         * Returns the parent builder, for further configuration.
+         * Example: After calling this you could now 
+         * use the DriverConfigBuilder if needed. 
+         * 
+         * @return The parent builder.
+         */
         public StoredConnectionBuilder done()
         {
             return parent;
         }
         
+        /**
+         * Sets the property of the DataSource for a StoredConnection's Driver.
+         * Example: assigning the Oracle Thin Driver's 'explicitCachingEnabled' property to 'true'.
+         * 
+         * @param name The property name.
+         * @param value The value to set.
+         * @return This builder.
+         */
         public DataSourceConfigBuilder property(String name, Object value)
         {
             config.put(name, value);
